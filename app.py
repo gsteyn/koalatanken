e67b8ee951c6c72f33f0d24b73eb11357531b222
Gareth Steyn <steyn.gareth@gmail.com>
2016-12-17 20:44:33 +0100

    initial radius search

--- app.py
+++ app.py
@@ -20,28 +20,50 @@ def retrieve_soup(url, parser='html.parser'):
     soup = BeautifulSoup(request.read(), parser)
     return soup
 
+
 '''
 DB related functions
 '''
 
+
 def initialize_db():
-    return MongoClient().test_database
+    return MongoClient().koala_tanken
+
 
 def insert_record(record):
     db.stations.insert_one(record)
 
+
 def find_record(record):
     return db.stations.find_one(record)
+
 
 def find_all_records(record):
     return db.stations.find(record)
 
+
 def remove_all():
     db.stations.delete_many({})
+
+
+def create_index():
+    try:
+        db.koala_tanken.createIndex({
+            'loc': '2dsphere'
+        })
+    except Exception as e:
+        print(e)
+
 
 def print_db_contents():
     for record in db.stations.find():
         pprint(record)
+
+
+def miles_to_radian(miles):
+    earthRadiusInMiles = 3959;
+    return miles / earthRadiusInMiles
+
 
 '''
 > Main procedure code
@@ -51,30 +73,56 @@ BASE_URL = 'http://www.brandstof-zoeker.nl'
 
 # initializes db
 db = initialize_db()
-remove_all()
+# remove_all()
 
 # initializes the google maps api
 gmaps = googlemaps.Client(key='AIzaSyCR883xLQrbS98hEshOePFIlrc9vaf9Cr4')
 
 with open('content/data/fuel_1.json') as data_file:
     data = json.load(data_file)
-    data['location'] = gmaps.geocode(data['addressLine2'] + ', ' + data['postalCode'])[0]['geometry']['location']
+    location = gmaps.geocode(data['addressLine'] + ', ' + data['postalCode'])[0]['geometry']['location']
+    coords = [location['lng'], location['lat']]
+    data['loc'] = {
+        'type': 'Point',
+        'coordinates': coords
+    }
     insert_record(data)
 
 with open('content/data/fuel_2.json') as data_file:
     data = json.load(data_file)
-    data['location'] = gmaps.geocode(data['addressLine2'] + ', ' + data['postalCode'])[0]['geometry']['location']
+    location = gmaps.geocode(data['addressLine'] + ', ' + data['postalCode'])[0]['geometry']['location']
+    coords = [location['lng'], location['lat']]
+    data['loc'] = {
+        'type': 'Point',
+        'coordinates': coords
+    }
     insert_record(data)
 
 with open('content/data/fuel_3.json') as data_file:
     data = json.load(data_file)
-    data['location'] = gmaps.geocode(data['addressLine2'] + ', ' + data['postalCode'])[0]['geometry']['location']
+    location = gmaps.geocode(data['addressLine'] + ', ' + data['postalCode'])[0]['geometry']['location']
+    coords = [location['lng'], location['lat']]
+    data['loc'] = {
+        'type': 'Point',
+        'coordinates': coords
+    }
     insert_record(data)
+
+
+print_db_contents()
+create_index()
+
+db.koala_tanken.find({
+    'loc': {
+        '$geoWithin': {
+            '$centerSphere': [[4.339181, 52.089521], miles_to_radian(5)]
+        }
+    }
+}).pretty()
 
 # for detail in find_all_records({'fuelDetails.moreOrLess': {'$gt' : 0}}):
 #     pprint(detail)
 
-print_db_contents()
 
 # ensures that the url doesn't end in '/ or ’/ as these url's go back to the main page
 pattern = re.compile('(?!.*\'/$)(?!.*’/$)(?!.*0/$)')
@@ -83,86 +131,97 @@ mainPage = retrieve_soup(BASE_URL + '/station/')
 letterListDiv = mainPage.find_all('div', attrs={'class': 'left'})
 
 # get links from the main/index page
-links = []
-for div in letterListDiv:
-    anchors = div.findAll('a')
-    for a in anchors:
-        href = a['href']
-        if pattern.match(href):
-            links.append(a['href'])
+# links = []
+# for div in letterListDiv:
+#     anchors = div.findAll('a')
+#     for a in anchors:
+#         href = a['href']
+#         if pattern.match(href):
+#             links.append(a['href'])
+#
+# # get all the links to the station details page
+# stationLinks = []
+# for link in links:
+#     stationListPage = retrieve_soup(BASE_URL + link)
+#     stationListDiv = stationListPage.findAll('div', attrs={'class': 'left'})
+#     for div in stationListDiv:
+#         anchors = div.findAll('a')
+#         for a in anchors:
+#             href = a['href']
+#             if '/station' in href:
+#                 stationLinks.append(href)
+#
+#
+# # scrape details of ALL stations
+# print(len(stationLinks))
+# stationIndex = 0
+# for stationLink in stationLinks:
+#     fullUrl = BASE_URL + stationLink
+#     print(stationIndex)
+#     print(fullUrl)
+#     stationIndex += 1
+#
+#     stationSoup = retrieve_soup(fullUrl)
+#
+#     # get DOM of the details div
+#     stationDetailsDivs = stationSoup.findAll('div', attrs={'class': 'left'})
+#     for div in stationDetailsDivs:
+#         # get fuel station address details
+#         half1Div = div.findAll('div', attrs={'class': 'half1'})
+#         addressContents = half1Div[0].contents
+#         name = addressContents[2].strip(' \t\n\r')
+#         addressLine = addressContents[4].strip(' \t\n\r')
+#         anchors = half1Div[0].findAll('a')
+#         postalCode = anchors[0].getText() + ' ' + anchors[1].getText()
+#
+#         location = {}
+#         coords = []
+#         try:
+#             location = gmaps.geocode(addressLine + ', ' + postalCode)[0]['geometry']['location']
+#             print(location)
+#             coords.append(location['lng'])
+#             coords.append(location['lat'])
+#         except Exception as e:
+#             print(e)
+#
+#         fuelStation = {
+#             'name': name,
+#             'addressLine': addressLine,
+#             'postalCode': postalCode,
+#             'loc': {
+#                 'type': 'Point',
+#                 'coordinates': coords
+#             },
+#             'fuelDetails': []
+#         }
+#
+#         # getting the fuel details
+#         half2Div = div.findAll('div', attrs={'class': 'half2'})
+#         if len(half2Div) == 0:
+#             pprint(fuelStation)
+#             insert_record(fuelStation)
+#             continue
+#
+#         fuelTypeTags = half2Div[0].findAll('dt')
+#         fuelPriceTags = half2Div[0].findAll('dd')
+#         for index, fuelTypeTag in enumerate(fuelTypeTags):
+#             # get the fuel type
+#             fuelType = fuelTypeTag.getText().strip()
+#             # get the prices and more-or-less status of the first fuel type
+#             fuelPriceStr = fuelPriceTags[index].getText()
+#             prices = re.findall(r'\d+\.*\d*', fuelPriceStr)
+#             fuelPrice = float(prices[0])
+#             moreOrLessPrice = float(prices[len(prices) - 1])
+#             isLess = 'goedkoper' in fuelPriceStr
+#             moreOrLessPrice = moreOrLessPrice * -1 if isLess else moreOrLessPrice
+#             # add fuel price details to fuelstation object
+#             fuelStation['fuelDetails'].append({
+#                 'fuelType': fuelType,
+#                 'price': fuelPrice,
+#                 'moreOrLess': moreOrLessPrice
+#             })
+#
+#         pprint(fuelStation)
+#         insert_record(fuelStation)
 
-# get all the links to the station details page
-stationLinks = []
-for link in links:
-    stationListPage = retrieve_soup(BASE_URL + link)
-    stationListDiv = stationListPage.findAll('div', attrs={'class': 'left'})
-    for div in stationListDiv:
-        anchors = div.findAll('a')
-        for a in anchors:
-            href = a['href']
-            if '/station' in href:
-                stationLinks.append(href)
-
-
-for station in stationLinks:
-    json_station = {'station': station}
-    insert_record(json_station)
-
-for post in find_record({'station': '/station/total-sss-de-stoven-zutphen-2037/'}):
-    print(post)
-
-# scrape details of ALL stations
-print(len(stationLinks))
-stationIndex = 0
-for stationLink in stationLinks:
-    fullUrl = BASE_URL + stationLink
-    print(stationIndex)
-    print(fullUrl)
-    stationIndex += 1
-
-    stationSoup = retrieve_soup(fullUrl)
-
-    # get DOM of the details div
-    stationDetailsDivs = stationSoup.findAll('div', attrs={'class': 'left'})
-    for div in stationDetailsDivs:
-        # get fuel station address details
-        half1Div = div.findAll('div', attrs={'class': 'half1'})
-        addressContents = half1Div[0].contents
-        name = addressContents[2].strip(' \t\n\r')
-        addressLine = addressContents[4].strip(' \t\n\r')
-        anchors = half1Div[0].findAll('a')
-        postalCode = anchors[0].getText() + ' ' + anchors[1].getText()
-
-        fuelStation = {
-            'name': name,
-            'addressLine': addressLine,
-            'postalCode': postalCode,
-            'fuelDetails': []
-        }
-
-        # getting the fuel details
-        half2Div = div.findAll('div', attrs={'class': 'half2'})
-        if len(half2Div) == 0:
-            pprint(fuelStation)
-            continue
-
-        fuelTypeTags = half2Div[0].findAll('dt')
-        fuelPriceTags = half2Div[0].findAll('dd')
-        for index, fuelTypeTag in enumerate(fuelTypeTags):
-            # get the fuel type
-            fuelType = fuelTypeTag.getText().strip()
-            # get the prices and more-or-less status of the first fuel type
-            fuelPriceStr = fuelPriceTags[index].getText()
-            prices = re.findall(r'\d+\.*\d*', fuelPriceStr)
-            fuelPrice = float(prices[0])
-            moreOrLessPrice = float(prices[len(prices) - 1])
-            isLess = 'goedkoper' in fuelPriceStr
-            moreOrLessPrice = moreOrLessPrice * -1 if isLess else moreOrLessPrice
-            # add fuel price details to fuelstation object
-            fuelStation['fuelDetails'].append({
-                'fuelType': fuelType,
-                'price': fuelPrice,
-                'moreOrLess': moreOrLessPrice
-            })
-
-        pprint(fuelStation)
+# ensure_index()
